471ca677818f3518476ad46228ba75bfcbb6f59f
williamrolando88 <william.rolando88@hotmail.com>
2022-05-05 15:59:56 -0500

    Add: Show password option

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,36 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="flex flex-col h-screen w-screen justify-center items-center">
       <div className="-mt-10">
         <img src="Hotelzilla-logo.png" alt="" width="200" />
-        <form action="#" method="post">
-          <label htmlFor="name">Name:</label> <br />
-          <input
-            className="border rounded px-2 py-1"
-            name="name"
-            id="name"
-            type="text"
-          />
-          <br />
-          <label htmlFor="email">Email:</label> <br />
-          <input
-            className="border rounded px-2 py-1"
-            name="email"
-            id="email"
-            type="email"
-          />
-          <br />
-          <label htmlFor="password">Password:</label> <br />
-          <input
-            className="border rounded px-2 py-1"
-            name="password"
-            id="password"
-            type="password"
-          />
-          <br />
-          <button type="submit">Sign Up</button>
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <label htmlFor="name">Name:</label>
+            <input
+              className="border rounded px-2 py-1"
+              name="name"
+              id="name"
+              type="text"
+              value={form.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="email">Email:</label>
+            <input
+              className="border rounded px-2 py-1"
+              name="email"
+              id="email"
+              type="email"
+              value={form.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="password">Password:</label>
+            <input
+              className="border rounded px-2 py-1"
+              name="password"
+              id="password"
+              type={showPassword ? 'text' : 'password'}
+              value={form.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="flex justify-between items-center">
+            <label htmlFor="show-password">Show password?</label>
+            <input
+              type="checkbox"
+              name="show-password"
+              id="show-password"
+              checked={showPassword}
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          </div>
+          <button className="bg-green-500 w-full rounded-md py-1" type="submit">
+            Sign Up
+          </button>
         </form>
       </div>
     </div>
