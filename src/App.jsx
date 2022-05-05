7986c4d5be912e9d8e8f02f173d32b911fc38d7e
George Magdy <georgtriple@gmail.com>
2022-05-05 19:44:51 +0200

    Add login route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Test from './components/Test';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Test />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
