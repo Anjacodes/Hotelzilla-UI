906d23c4306822a1bcf3461ade812e0a47f83c31
George Magdy <georgtriple@gmail.com>
2022-05-05 19:44:23 +0200

    Add login reducer to store

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import loginReducer from './login/login';
 import logger from 'redux-logger';
 
 // add here the reducers!!
-const reducer = {};
+const reducer = {
+  token: loginReducer,
+};
 
 const store = configureStore({
   reducer,
