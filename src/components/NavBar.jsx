ac4c493a67201be507b12713371de9a94a5b0fe2
Anjacodes <anja_schmidt7@posteo.de>
2022-05-09 15:34:49 -0500

    Implement Suggestions from PR

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,12 +13,12 @@ const role = getRole(token)
   return (
     <nav className='w-[20vw] h-[100vh] flex flex-col items-center justify-evenly py-4 border-r'>
       <a href='/'><img src="Hotelzilla-logo.png" alt="Hotelzilla Logo" className='h-[25vh]'/></a>
-      <ul className='flex flex-col mt-12 text-gray-600'>
+      <div className='flex flex-col mt-12 text-gray-600'>
          { renderAddDeleteHotel(role) }
          { renderReserve(loggedIn) }
          <NavLink to="/login" className="py-2 font-bold hover:bg-lime-400 text-xl font-[Taxicab]">{loggedIn ? "LOG OUT" : "LOG IN"}</NavLink>
          { renderSignUpButton(loggedIn) }
-      </ul>
+      </div>
       <footer className='mt-auto flex flex-col items-center'>
         <div className='flex w-full justify-around mb-2'>
         <i className="fa-brands fa-twitter"></i>
