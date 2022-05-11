bc4e981daaefeaa7290c861e0ffb0d75075a92c0
williamrolando88 <william.rolando88@hotmail.com>
2022-05-11 16:22:39 -0500

    Add: Action to fetch all hotels

--- src/redux/hotels/hotelsSlice.js
+++ src/redux/hotels/hotelsSlice.js
@@ -1,12 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getAllHotels } from './hotelsHelper';
 
-const initialState = {};
+const initialState = {
+  getHotels: '',
+  hotels: [],
+};
 
 const hotelsSlice = createSlice({
   name: 'hotels',
   initialState,
-  reducers: {},
-  extraReducers: {},
+  reducers: {
+    resetStatus(state) {
+      state.getHotels = '';
+    },
+  },
+  extraReducers: {
+    [getAllHotels.fulfilled]: (state, action) => {
+      state.getHotels = 'fulfilled';
+      state.hotels = action.payload;
+    },
+  },
 });
 
 export default hotelsSlice.reducer;
+
+export const { resetStatus } = hotelsSlice.actions;
