9fba10e1746d55336cda26a2cce882cc7879084e
Anjacodes <anja_schmidt7@posteo.de>
2022-05-11 16:55:38 -0500

    Remove Commented Code

--- src/modules/printStars.jsx
+++ src/modules/printStars.jsx
@@ -3,19 +3,6 @@ import Box from "@mui/material/Box";
 import StarIcon from "@mui/icons-material/Star";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 
-// const printStars = (num) => {
-//     let string = ''
-//     while (num > 0) {
-//       string += "⭐️";
-//       num--;
-//       if (num === 0.5) {
-//         string+="✩";
-//         num = num - 0.5
-//       }
-//     }
-//     return string;
-//   }
-
 const printStars = (num) => {
   const totalStars = 5;
   let activeStars = num;
