7cd4a33ff11b11f02180ef6909d86295fc8d53a2
williamrolando88 <william.rolando88@hotmail.com>
2022-05-12 10:34:39 -0500

    Fix "Admin" value typo

--- src/components/accessibility/isAdmin.jsx
+++ src/components/accessibility/isAdmin.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 
-function IsAdmin({role, loggedIn}) {
-  if (role !== "admin" && !loggedIn) return <Navigate to="/" />;
+function IsAdmin({ role, loggedIn }) {
+  if (role !== 'Admin' && !loggedIn) return <Navigate to="/" />;
   return <Outlet />;
 }
 
