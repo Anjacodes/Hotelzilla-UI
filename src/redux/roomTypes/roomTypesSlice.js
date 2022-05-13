cd9089b7213e4254b9447ad91e00ac16c95b4137
williamrolando88 <william.rolando88@hotmail.com>
2022-05-13 11:53:36 -0500

    Add actions to `roomType` reducer
    `getRoomTypes` that fetch data from API
    `resetGetRoomTypesStatus` that reset API call status

--- src/redux/roomTypes/roomTypesSlice.js
+++ src/redux/roomTypes/roomTypesSlice.js
@@ -1,12 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import registerSlice from '../register/registerSlice';
 
-const initialState = {};
+const initialState = {
+  getRoomStatus: '',
+  types: [],
+};
+
+export const getRoomTypes = createAsyncThunk('getRoomTypes', async (token) => {
+  const url = '';
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: { Authentication: token },
+  });
+  return response.json();
+});
 
 const roomTypesSlice = createSlice({
   name: 'roomTypes',
   initialState,
-  reducers: {},
-  extraReducers: {},
+  reducers: {
+    resetGetRoomTypesStatus(state) {
+      state.getRoomStatus = '';
+    },
+  },
+  extraReducers: {
+    [getRoomTypes.pending]: (state) => {
+      state.getRoomStatus = 'pending';
+    },
+    [getRoomTypes.rejected]: (state) => {
+      state.getRoomStatus = 'rejected';
+    },
+    [getRoomTypes.fulfilled]: (state, action) => {
+      state.getRoomStatus = 'fulfilled';
+      state.types = action.payload;
+    },
+  },
 });
 
 export default roomTypesSlice.reducer;
+
+export const { resetGetRoomTypesStatus } = roomTypesSlice.actions;
