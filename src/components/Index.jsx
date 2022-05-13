8e2023434a7ff401b747600be8eb2c2dc81dab0e
Anjacodes <anja_schmidt7@posteo.de>
2022-05-13 17:41:40 -0500

    Buttons inactive when no next page

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -11,7 +11,8 @@ function Index() {
 
   let [currentPage, setCurrentPage] = useState(1);
   let [ hotelsPerPage ] = useState(3);
-  const [inactive, setInactive] = useState(true);
+  const [inactiveUp, setInactiveUp] = useState(true);
+  const [inactiveDown, setInactiveDown] = useState(false);
 
   useEffect(() => {
     dispatch(getAllHotels());
@@ -22,29 +23,36 @@ function Index() {
   const indexOfFirstHotel = indexOfLastHotel - hotelsPerPage;
   const currentHotels = hotels.slice(indexOfFirstHotel, indexOfLastHotel);
 
+  useEffect(() => {
+    if (currentPage === 1) {
+      setInactiveDown(true)
+    } else {
+      setInactiveDown(false)
+    }
+    if (indexOfLastHotel < hotels.length-1) {
+      setInactiveUp(false)
+    } else {
+      setInactiveUp(true)
+    }
+  }, [currentHotels])
+
   // Change pages
   const handlePageUp = () => {
-    if (currentHotels.length < hotelsPerPage) {
-      setInactive(true);
-      return
+    if (indexOfLastHotel < hotels.length-1) {
+      setCurrentPage((page) => page +1);
     }
-    setInactive(false);
-    setCurrentPage((page) => page +1);
   }
 
   const handlePageDown = () => {
-    if (currentPage === 1) {
-      setInactive(true)
-      return
+    if (currentPage > 1) {
+      setCurrentPage((page) => page -1)
     }
-    setInactive(false)
-    setCurrentPage((page) => page -1)
   }
 
   return (
     <>
-      <div className="flex flex-col w-[80vw]">
-        <div className="flex justify-end p-5">
+      <div className="flex flex-col w-[80vw] justify-center">
+        <div className="flex justify-end p-5 pb-12">
           {role === 'Admin' && (
             <NavLink
               to="/add-hotel"
@@ -54,15 +62,14 @@ function Index() {
             </NavLink>
           )}
         </div>
-        <div className="grid grid-cols-3 grid-rows-3 gap-6 p-5">
+        <div className="grid grid-cols-3 grid-rows-1 gap-6 p-5">
           {currentHotels.map((hotel) => (
             <HotelItem key={hotel.id} hotel={hotel} />
           ))}
         </div>
       </div>
-      <i className={inactive ? "not:hover fixed left-[20vw] top-[50vh] py-4 px-6 bg-gray-200 rounded-r-full fa-solid fa-caret-left text-slate-50" : "fixed left-[20vw] top-[50vh] py-4 px-6 bg-gray-200 hover:bg-lime-500 rounded-r-full fa-solid fa-caret-left text-slate-50"} onClick={handlePageDown}/>
-      <i className={inactive ? "not:hover fixed right-0 top-[50vh] py-4 px-6 bg-lime-400 rounded-l-full fa-solid fa-caret-right text-slate-50" : "fixed right-0 top-[50vh] py-4 px-6 bg-lime-400 hover:bg-gray-200 rounded-l-full fa-solid fa-caret-right text-slate-50"}
-       onClick={handlePageUp}/>
+      <i className={`fixed left-[20vw] top-[50vh] py-4 px-6 bg-gray-200 rounded-r-full fa-solid fa-caret-left text-slate-50 ${inactiveDown ? "hover:not" : "hover:bg-lime-400"}`} onClick={handlePageDown}/>
+      <i className={`fixed right-0 top-[50vh] py-4 px-6 bg-lime-400 rounded-l-full fa-solid fa-caret-right text-slate-50 ${inactiveUp ? "hover:not" : "hover:bg-gray-200"}`} onClick={handlePageUp}/>
     </>
   );
 }
