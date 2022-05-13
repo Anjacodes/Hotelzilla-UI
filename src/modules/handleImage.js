15a288fd5f0e5696e06e824cc8e968193289e845
Anjacodes <anja_schmidt7@posteo.de>
2022-05-13 11:03:27 -0500

    Fix Linters

--- src/modules/handleImage.js
+++ src/modules/handleImage.js
@@ -1,7 +1,5 @@
 const hostURL = 'https://hotelzilla-api.herokuapp.com';
 
-const handleImage = (img) => {
-  return img ? `${hostURL}${img.url}` : `https://images.unsplash.com/photo-1517840901100-8179e982acb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80`
-};
+const handleImage = (img) => (img ? `${hostURL}${img.url}` : 'https://images.unsplash.com/photo-1517840901100-8179e982acb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
 
 export default handleImage;
