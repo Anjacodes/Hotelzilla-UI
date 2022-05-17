68127c40e15be90cfdebd75a191c712d8279ef35
williamrolando88 <william.rolando88@hotmail.com>
2022-05-16 20:38:45 -0500

    Add back button
    Delete styling from table element

--- src/components/removeHotel/RemoveHotel.jsx
+++ src/components/removeHotel/RemoveHotel.jsx
@@ -4,6 +4,7 @@ import { getAllHotels, resetDeleteStatus } from '../../redux/hotel/hotel';
 import { deleteHotel } from '../../redux/hotel/hotel-helper';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import ConfirmDelete from './ConfirmDelete';
+import BackButton from '../navigation/BackButton';
 
 const RemoveHotel = () => {
   const dispatch = useDispatch();
@@ -44,7 +45,7 @@ const RemoveHotel = () => {
   }, []);
 
   return (
-    <section className="flex w-full flex-col items-center">
+    <>
       {deleteStatus === 'fulfilled' && (
         <div className="absolute bottom-4 right-4 z-10 rounded  bg-green-200 px-4 py-2 text-green-700">
           Hotel successfully deleted!
@@ -55,43 +56,42 @@ const RemoveHotel = () => {
           Ups! Something went wrong
         </div>
       )}
-      <header>
-        <h2 className="mt-10 mb-10 font-Taxicab text-3xl text-gray-800 md:mt-[25vh]">
-          Delete Hotel
-        </h2>
-      </header>
-      <table className="mx-auto w-4/5 text-gray-500 md:table-auto">
-        <thead className="bg-gray-500 font-Taxicab text-lg uppercase text-slate-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th className="px-6 py-3 text-left">Item</th>
-            <th className="w-96 py-3 px-6 text-left">Hotel name</th>
-            <th className="py-3 px-6 ">Action</th>
-          </tr>
-        </thead>
-        <tbody className="font-Metrophobic">
-          {hotels.map((hotel, index) => (
-            <tr key={index} className="even:bg-white">
-              <td className="px-6 py-4">{index + 1}</td>
-              <td className="px-6 py-4 text-neutral-900">{hotel.name}</td>
-              <td className="flex items-center justify-center py-4">
-                <button
-                  className="rounded-md bg-red-500 px-2 py-1 text-white hover:bg-red-200 hover:text-red-800"
-                  onClick={() => handleClick(hotel)}
-                >
-                  <DeleteForeverIcon />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <ConfirmDelete
-        visible={modalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-        hotel={modalInfo}
-      />
-    </section>
+      <section className="flex h-screen">
+        <div className="flex flex-col items-center gap-3 p-9">
+          <img className="w-4/12" src="Hotelzilla-logo.png" alt="" />
+          <h2 className="text-2xl capitalize">Delete hotel</h2>
+          <table className="h-[40vh] overflow-y-auto overscroll-contain">
+            <thead className="font-Taxicab uppercase">
+              <tr>
+                <th className="">Item</th>
+                <th className="">Hotel name</th>
+                <th className="">Action</th>
+              </tr>
+            </thead>
+            <tbody className="h-[40vh] ">
+              {hotels.map((hotel, index) => (
+                <tr key={index} className="even:bg-white">
+                  <td className="py-3 px-6">{index + 1}</td>
+                  <td className="py-3 px-6">{hotel.name}</td>
+                  <td className="py-3 px-6">
+                    <button className="" onClick={() => handleClick(hotel)}>
+                      <DeleteForeverIcon />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <ConfirmDelete
+            visible={modalVisible}
+            onOk={handleOk}
+            onCancel={handleCancel}
+            hotel={modalInfo}
+          />
+        </div>
+      </section>
+      <BackButton />
+    </>
   );
 };
 
